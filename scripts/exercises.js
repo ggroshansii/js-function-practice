b1fb3ab33d5be1b7e4fd31317d2b94686e20ef3f
ggroshansii <garthgroshansii@gmail.com>
2021-08-31 13:49:15 -0400

    completed add and multiply JS exercises

--- scripts/exercises.js
+++ scripts/exercises.js
@@ -83,6 +83,21 @@
     // Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24.
     // ---------------------
 
+      function sum(arr) {
+        let sum = 0;
+        for (let i = 0; i < arr.length; i++) {
+          sum += arr[i];
+        }
+        return sum;
+      }
+
+      function multiply(arr) {
+        let product = 1;
+        for (let i = 0; i < arr.length; i++) {
+          product *= arr[i];
+        }
+        return product;
+      }
 
 
     // ---------------------
